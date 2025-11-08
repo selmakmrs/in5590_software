94dfa631a86bed3a318b9b218e8e2ceb28f31b3d
Selma <selma.storstad@gmail.com>
2025-11-08 14:07:00 +0100

    wheel movemnts

--- body/body.py
+++ body/body.py
@@ -489,7 +489,7 @@ if __name__ == "__main__":
         seconds = [0.5, 1.0, 1.5, 2, 2.5, 3]
         body.set_wheel_mode()
         for sec in seconds[:-1]:
-            body.set_joint_mode()
+            body.set_wheel_mode()
             print(f"Second = {sec}")
             time.sleep(1)
             body.wheel_speed(BASE_ID, FAST)
