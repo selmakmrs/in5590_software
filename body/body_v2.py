29ededbde2b474b1da91b6d38db24449fd7173a3
Selma <selma.storstad@gmail.com>
2025-11-09 18:51:00 +0100

    servo noise test

--- body/body_v2.py
+++ body/body_v2.py
@@ -432,9 +432,9 @@ class BODY:
 
         duration = 0.7
 
-        base_config = (BASE_ID, -1024, 0.4)
-        body_config = (BODY_ID, 1024, 0.3)
-        head_config = (HEAD_ID, -1024, 0.3)
+        base_config = (BASE_ID, -1024, 0.6)
+        body_config = (BODY_ID, 1024, 0.9)
+        head_config = (HEAD_ID, -1024, 0.6)
 
         self._run_wheel_movements([base_config, body_config, head_config])
         self.home_position()
