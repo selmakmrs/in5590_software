430333b6f24a9e69bf5e668065077610dc284df2
Selma <selma.storstad@gmail.com>
2025-11-09 17:21:47 +0100

    servo noise test

--- body/body_v2.py
+++ body/body_v2.py
@@ -256,16 +256,16 @@ class BODY:
         steps = 20
         duration = 0.01
 
-        head_config = (HEAD_ID, self.tracked_positions[HEAD_ID], 20, 200)
-        body_config = (BODY_ID, self.tracked_positions[HEAD_ID], 200, 200)
+        head_config = (HEAD_ID, self.tracked_positions[HEAD_ID], 20, 150)
+        body_config = (BODY_ID, self.tracked_positions[HEAD_ID], 200, 150)
         self.move_positions_smooth(layer_configs=[head_config, body_config], steps=steps, duration=duration)
 
         # Hold position
         time.sleep(0.5)
 
         # Return home
-        head_config = (HEAD_ID, self.tracked_positions[HEAD_ID], HOME_POSITIONS[HEAD_ID], 200)
-        body_config = (BODY_ID, self.tracked_positions[HEAD_ID], HOME_POSITIONS[BODY_ID], 200)
+        head_config = (HEAD_ID, self.tracked_positions[HEAD_ID], HOME_POSITIONS[HEAD_ID], 150)
+        body_config = (BODY_ID, self.tracked_positions[HEAD_ID], HOME_POSITIONS[BODY_ID], 150)
         self.move_positions_smooth(layer_configs=[head_config, body_config], steps=steps, duration=duration)
 
 
