71334db4aa8001c10a08fcc8a51dc2140dea0248
Selma <selma.storstad@gmail.com>
2025-11-16 16:47:04 +0100

    test onnoc

--- camera/onnx_test.py
+++ camera/onnx_test.py
@@ -10,7 +10,7 @@ from picamera2 import Picamera2
 
 picam2 = Picamera2()
 
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
+config = picam2.create_preview_configuration(main={"size": (340, 240)})
 picam2.configure(config)
 picam2.start()
 
